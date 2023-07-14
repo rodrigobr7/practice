77b5ba56329941d61d6519dc82f429f39fe41299
Rodrigobr7 <rodrigobr731@gmail.com>
2023-07-14 13:20:15 +0100

    first project

---
+++ index.js
@@ -0,0 +1,8 @@
+const btn = document.querySelector("button");
+
+function tutor() {
+    const name = prompt("Enter your name here:");
+    alert(`Thank you Tutor ${name}, for introducing me to this madness! 
+    Valeu!!!🤘🏾🏄🏾‍♂️🍻`);
+}
+btn.addEventListener("click", tutor);
